9eb9442a1f252b20f9e462a7e68657e69d701cc9
Sebastian Jensen <apps@tsjdev-apps.de>
2024-04-14 19:36:27 +0200

    Add logic to chat with your data.

--- src/CopilotOnData/Program.cs
+++ src/CopilotOnData/Program.cs
@@ -1,4 +1,8 @@
-﻿using CopilotOnData.Services;
+﻿using Azure;
+using Azure.AI.OpenAI;
+using CopilotOnData.Services;
+using Spectre.Console;
+using System.Text;
 
 // Get Endpoint
 string openAiEndpoint = 
@@ -32,7 +36,76 @@ string cosmosDbDatabaseName =
 string cosmosDbContainerName = 
     ConsoleService.GetString("Please insert the [yellow]container name[/]:");
 
+// Get Container name
+string cosmosDbContentFieldName =
+    ConsoleService.GetString("Please insert the [yellow]name of the content field[/]:");
+
+// Get Container name
+string cosmosDbVectorFieldName =
+    ConsoleService.GetString("Please insert the [yellow]name of the vector field[/]:");
+
 // Show Header
 ConsoleService.CreateHeader();
 
-Console.ReadKey();
+// Create OpenAI Client
+OpenAIClient openAIClient = new(new Uri(openAiEndpoint), new AzureKeyCredential(azureOpenAiKey));
+
+// Create ChatCompletionsOptions
+ChatCompletionsOptions options = CreateChatCompletionsOptions();
+
+// Chat implementation
+while (true)
+{
+    AnsiConsole.WriteLine();
+    AnsiConsole.MarkupLine("[green]User:[/]");
+
+    var userMessage = Console.ReadLine();
+    options.Messages.Add(new ChatRequestUserMessage(userMessage));
+
+    AnsiConsole.WriteLine();
+    AnsiConsole.MarkupLine("[green]Copilot on Data:[/]");
+
+    StringBuilder stringBuilder = new();
+    await foreach (var chatUpdate in await openAIClient.GetChatCompletionsStreamingAsync(options))
+    {
+        if (chatUpdate.ChoiceIndex.HasValue && chatUpdate.ContentUpdate is not null)
+        {
+            AnsiConsole.Write(chatUpdate.ContentUpdate);
+            stringBuilder.Append(chatUpdate.ContentUpdate);
+        }
+    }
+
+    AnsiConsole.WriteLine();
+    options.Messages.Add(new ChatRequestAssistantMessage(stringBuilder.ToString()));
+}
+
+ChatCompletionsOptions CreateChatCompletionsOptions()
+{
+    AzureCosmosDBChatExtensionConfiguration dbChatConfiguration = new()
+    {
+        Authentication = new OnYourDataConnectionStringAuthenticationOptions(cosmosDbConnectionString),
+        IndexName = cosmosDbIndexName,
+        DatabaseName = cosmosDbDatabaseName,
+        ContainerName = cosmosDbContainerName,
+        FieldMappingOptions = new AzureCosmosDBFieldMappingOptions(
+            [cosmosDbContentFieldName], [cosmosDbVectorFieldName]),
+        ShouldRestrictResultScope = true,
+        DocumentCount = 5,
+        Strictness = 1,
+        RoleInformation = "You are an AI assistant to help with the provided data.",
+        VectorizationSource = new OnYourDataDeploymentNameVectorizationSource(embeddingsDeploymentName)
+    };
+
+    AzureChatExtensionsOptions azureChatExtensionsOptions = new();
+    azureChatExtensionsOptions.Extensions.Add(dbChatConfiguration);
+
+    ChatCompletionsOptions chatCompletionsOptions = new()
+    {
+        MaxTokens = 1000,
+        Temperature = 0.7f,
+        DeploymentName = chatDeploymentName,
+        AzureExtensionsOptions = azureChatExtensionsOptions
+    };
+
+    return chatCompletionsOptions;
+}
