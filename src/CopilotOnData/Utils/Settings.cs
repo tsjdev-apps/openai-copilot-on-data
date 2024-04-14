5f8111633b3ac6c3283c9504d0f5e7f38d280541
Sebastian Jensen <apps@tsjdev-apps.de>
2024-04-14 20:25:14 +0200

    Create Settings class to predefine parameter values.

---
+++ src/CopilotOnData/Utils/Settings.cs
@@ -0,0 +1,18 @@
+﻿namespace CopilotOnData.Utils;
+
+internal static class Settings
+{
+    // OPEN AI SETTINGS
+    public const string? OpenAiEndpoint = "";
+    public const string? OpenAiKey = "";
+    public const string? ChatDeploymentName = "";
+    public const string? EmbeddingDeploymentName = "";
+
+    // COSMOS DB SETTINGS
+    public const string? CosmosDbConnectionString = "";
+    public const string? CosmosDbIndexName = "";
+    public const string? CosmosDbDatabaseName = "";
+    public const string? CosmosDbContainerName = "";
+    public const string? CosmosDbContentFieldName = "";
+    public const string? CosmosDbVectorFieldName = "";
+}
