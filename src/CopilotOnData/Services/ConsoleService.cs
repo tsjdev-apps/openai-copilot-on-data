80135e5178947c9ff5644b135d8e375868730526
Sebastian Jensen <apps@tsjdev-apps.de>
2024-04-14 19:16:23 +0200

    Create ConsoleService to get and process user input.

---
+++ src/CopilotOnData/Services/ConsoleService.cs
@@ -0,0 +1,75 @@
+﻿using Spectre.Console;
+
+namespace CopilotOnData.Services;
+
+internal static class ConsoleService
+{
+    public static void CreateHeader()
+    {
+        AnsiConsole.Clear();
+
+        Grid grid = new();
+        grid.AddColumn();
+        grid.AddRow(new FigletText("Copilot on Data").Centered().Color(Color.Red));
+        grid.AddRow(Align.Center(new Panel("[red]Sample by Thomas Sebastian Jensen ([link]https://www.tsjdev-apps.de[/])[/]")));
+
+        AnsiConsole.Write(grid);
+        AnsiConsole.WriteLine();
+    }
+
+    public static string GetUrl(
+        string prompt)
+    {
+        CreateHeader();
+
+        return AnsiConsole.Prompt(
+            new TextPrompt<string>(prompt)
+            .PromptStyle("white")
+            .ValidationErrorMessage("[red]Invalid prompt[/]")
+            .Validate(prompt =>
+            {
+                if (prompt.Length < 3)
+                {
+                    return ValidationResult.Error("[red]Value too short[/]");
+                }
+
+                if (prompt.Length > 250)
+                {
+                    return ValidationResult.Error("[red]Value too long[/]");
+                }
+
+                if (Uri.TryCreate(prompt, UriKind.Absolute, out var uri)
+                    && uri.Scheme == Uri.UriSchemeHttps)
+                {
+                    return ValidationResult.Success();
+                }
+
+                return ValidationResult.Error("[red]No valid URL[/]");
+            }));
+    }
+
+    public static string GetString(
+        string prompt, bool limitLength = true)
+    {
+        CreateHeader();
+
+        return AnsiConsole.Prompt(
+            new TextPrompt<string>(prompt)
+            .PromptStyle("white")
+            .ValidationErrorMessage("[red]Invalid prompt[/]")
+            .Validate(prompt =>
+            {
+                if (prompt.Length < 3)
+                {
+                    return ValidationResult.Error("[red]Value too short[/]");
+                }
+
+                if (limitLength && prompt.Length > 200)
+                {
+                    return ValidationResult.Error("[red]Value too long[/]");
+                }
+                
+                return ValidationResult.Success();
+            }));
+    }
+}
